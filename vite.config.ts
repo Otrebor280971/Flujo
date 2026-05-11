932b1b53bc4adec37bf368e898f0b6b8ff6ef4df
Roberto Castro <roberto.castro.gomez.05@gmail.com>
2026-05-10 22:39:39 -0600

    Added logos

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Mi App de Finanzas',
+        short_name: 'Finanzas',
+        description: 'Control de gastos e ingresos',
+        theme_color: '#18181b', // Color de la barra de estado (ej. zinc-900)
+        background_color: '#18181b',
+        display: 'standalone', // Esto oculta la barra del navegador
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
